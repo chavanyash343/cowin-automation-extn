1488fc60c64c8163a210473d13aabf56f7341d1d
Sushrut Kasture <kasturesushrut@gmail.com>
2021-05-11 17:53:38 +0530

    Remove dependency on details.js

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,10 @@
+let mobilenumber = window.localStorage.getItem("mobile");
+let state_name = window.localStorage.getItem("state");
+let district_name = window.localStorage.getItem("district");
+let first_5_pin_digits = "";
+let allow_multiple = window.localStorage.getItem("allow_multiple")==="true"?true:false;
+console.log(typeof(allow_multiple));
+
 var waitForEl = function(selector, callback) {
   if ($(selector).length) {
     callback();
@@ -22,7 +29,7 @@ var waitForElAgain = function(selector, callback) {
 
 const repFun = () => {
   waitForEl("[formcontrolname=mobile_number]", function() {
-    $("[formcontrolname=mobile_number]").val(phone_number_9_digits);
+    $("[formcontrolname=mobile_number]").val(mobilenumber);
     $("[formcontrolname=mobile_number]").on('input', (e) => {
       if(e.target.value.length===10){
         $('.login-btn').trigger('click');
@@ -89,14 +96,6 @@ $(window).on("load", () => {
 });
 
 let focus_ids = ["[formcontrolname=otp]", "[formcontrolname=mobile_number]", "[formcontrolname=pincode]"];
-const keep_focusing = () => {
-  setInterval(()=>{
-    focus_ids.forEach(element => {
-      if($(element).length!==0) $(element).focus();
-    });
-    
-  }, 1000);  
-}
 
 if (window.location.hash) {
   $(window).trigger('hashchange')
@@ -143,4 +142,119 @@ setInterval(function(){
     }
 },100);
 
+
+const keep_focusing = () => {
+
+  setInterval(()=>{
+    focus_ids.forEach(element => {
+      
+      if($("#formWrapper").is(":hidden")) if($(element).length!==0) $(element).focus();
+    });
+    
+  }, 1000);  
+}
+
+
 keep_focusing();
+
+const createForm = () => {
+  let wrapperDiv = document.createElement("div");
+  let mobileInput = document.createElement("input");
+  let mobileLabel = document.createTextNode("Mobile number (first 9 digits): ");
+  let stateInput = document.createElement("input");
+  let stateLabel = document.createTextNode("Name of the state: ");
+  let districInput = document.createElement("input");
+  let districLabel = document.createTextNode("District name: ");
+
+  let allowMultipleInput = document.createElement("input");
+  allowMultipleInput.type = "checkbox";
+  allowMultipleInput.id = "allowMultiple";
+  let allowMultipleInputLabel = document.createTextNode("Allow multiple members");
+  let allowMultipleWarn = document.createElement('p');
+  allowMultipleWarn.appendChild(document.createTextNode("This will prevent automatic click on the Schedule Now button"));
+  allowMultipleWarn.style = "color: red;"
+
+  wrapperDiv.id = "formWrapper";
+  mobileInput.id = "data-mob";
+  stateInput.id = "data-state";
+  stateInput.value = state_name;
+  districInput.id = "data-district";
+  districInput.value = district_name;
+  mobileInput.type = "number";
+  mobileInput.value = mobilenumber;
+  allowMultipleInput.checked = allow_multiple;
+
+  let submitButton = document.createElement("button");
+  submitButton.id = "data-submit";
+  submitButton.appendChild(document.createTextNode("Save inputs"));
+  
+  wrapperDiv.style = "position: fixed; background: white; top: 12.5%; width: 75%; height: 75%; left: 12.5%; border: 3px solid #73AD21;"
+
+  wrapperDiv.appendChild(mobileLabel);
+  wrapperDiv.appendChild(mobileInput);
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+
+  wrapperDiv.appendChild(stateLabel);
+  wrapperDiv.appendChild(stateInput);
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+
+  wrapperDiv.appendChild(districLabel);
+  wrapperDiv.appendChild(districInput);
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(allowMultipleInputLabel);
+  wrapperDiv.appendChild(allowMultipleInput);
+  wrapperDiv.appendChild(document.createElement('br'));
+
+  wrapperDiv.appendChild(allowMultipleWarn);
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(document.createElement('br'));
+  wrapperDiv.appendChild(submitButton);
+
+
+  document.body.appendChild(wrapperDiv);
+}
+
+const createHideShowButton = () => {
+  $("#formWrapper").hide();
+  let formShowHide = document.createElement("button");
+  formShowHide.id = "formshowhidebutton";
+  formShowHide.appendChild(document.createTextNode("click to edit the autofill inputs"));
+  formShowHide.style = "background: red; position: sticky; top:0; left: 35%";
+  document.body.appendChild(formShowHide);
+  $('#formshowhidebutton').on('click', ()=>{
+    $("#formWrapper").toggle();
+  })
+}
+
+const bindSubmitButtonToSaveInfo = () => {
+  let submitbtn = document.getElementById("data-submit");
+  submitbtn.addEventListener("click", () => {
+    mobilenumber = document.getElementById("data-mob").value;
+    state_name = document.getElementById("data-state").value;
+    district_name = document.getElementById("data-district").value;
+    allow_multiple = document.getElementById("allowMultiple").checked;
+
+    console.log(allow_multiple);
+    $("#formWrapper").hide();
+    window.localStorage.setItem("mobile", mobilenumber);
+    window.localStorage.setItem("state", state_name);
+    window.localStorage.setItem("district", district_name);
+    window.localStorage.setItem("allow_multiple", allow_multiple);
+    window.location.reload();
+  })
+}
+
+const createFormAndOthers = () => {
+  createForm();
+  createHideShowButton();
+  bindSubmitButtonToSaveInfo();
+}
+
+createFormAndOthers();
